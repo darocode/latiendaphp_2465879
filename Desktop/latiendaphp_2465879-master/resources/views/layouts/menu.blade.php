348cea61ba2ed91a8406faaf8bb9504a7389208a
unknown <juan10448>
2022-05-10 11:52:24 -0500

    layouts y migraciones

---
+++ Desktop/latiendaphp_2465879-master/resources/views/layouts/menu.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('materialize/css/materialize.css') }}">
+</head>
+<body>
+
+    <nav class="blue-grey darken-4">
+        <div class="nav-wrapper ">
+            <a href="#" class="brand-logo">Logo</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="sass.html">Productos</a></li>
+                <li><a href="badges.html">Pedidos</a></li>
+                
+            </ul>
+        </div>
+    </nav>
+        <div class="container">
+            @yield('contenido')
+        </div>
+    <script src="{{ asset('materialize/js/materialize.js') }}"></script>
+</body>
+</html>
---
+++ Desktop/latiendaphp_2465879-master/resources/views/productos/new.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.menu')
+
+@section('contenido')
+
+<div class="row">
+    <h1 class="cyan-text text-accent-3">Nuevo Producto</h1>
+</div>
+<div class="row">
+    <form   class="col s8" 
+            method="POST" 
+            action="">
+        <div class="row">
+            <div class="input-field col s8">
+                <input  type="text" 
+                        placeholder="Nombre Producto"
+                        id="nombre"
+                        name="nombre">
+                        <label for="nombre">Nombre de producto</label>
+            </div>
+        </div>
+
+
+
+        <div class="row">
+            <div class="input-field col s8">
+                <textarea class="materialize-textarea"
+                            id="desc"
+                            name="desc"
+                            ></textarea>
+                            <label for="desc">Descripcion</label>
+            </div>
+        </div>
+
+        
+        <div class="row">
+            <div class="input-field col s8">
+                <input type="text"
+                        placeholder="Precio del producto"
+                        type="text"
+                        id="precio"
+                        name="precio">
+                    <label for="precio">Precio</label>
+            </div>
+        </div>
+        
+
+
+        <div class="row">
+            <div class="file-field input-field col s8">
+                <div class="btn">
+                    <span>File</span>
+                    <input type="file" name="imagen">
+                </div>
+                    <div class="file-path-wrapper">
+                        <input class="file-path validate" type="text">
+                    </div>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <button class="btn waves-effect waves-light col s8" type="submit" name="action">
+                Submit
+            </button>
+        </div>
+    </form>
+</div>
+@endsection
